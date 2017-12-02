5bc7cbeae941a470e27664b6b53f85e72b0cb38a
Bob Langley <bob.langley@particular.net>
2017-12-01 21:10:35 -0800

    Add web project

---
+++ Case00033133.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Case00033133.Web.Global" Language="C#" %>
---
+++ Case00033133.Web/Global.asax.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using NServiceBus;
+using StructureMap;
+
+namespace Case00033133.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private IBus bus;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var container = new Container();
+
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Case00033133");
+            busConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+
+            busConfiguration.UseContainer<StructureMapBuilder>(c => c.ExistingContainer(container));
+
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.EnableInstallers();
+
+
+            bus = Bus.Create(busConfiguration).Start();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            bus.Dispose();
+        }
+    }
+}
